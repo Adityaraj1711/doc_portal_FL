1040358f4916f6d56f676a6dca358884edd3db25
night knight <adityamailbox0@gmail.com>
2021-12-16 16:12:31 +0530

    deploy

--- src/admin.py
+++ src/admin.py
@@ -208,124 +208,124 @@ class MahindraCompleteDetailAdmin(ImportExportModelAdmin):
         return listDisplay
 
 
-class GeneralEntryAdmin(admin.ModelAdmin):
-    show_full_result_count = False
-    change_form_template = 'src/templates/admin/change_form.html'
-    search_fields = ('first_name', 'last_name', 'location__location')
-    list_display = ('first_name', 'last_name', 'location', )
-    actions = (ExportCsvMixin.export_as_csv, ExportAllCsvMixin.export_all_as_csv, )
-    list_per_page = 50
-    list_filter = (
-        ('date', DateFieldListFilter),
-    )
-    list_max_show_all = False
+# class GeneralEntryAdmin(admin.ModelAdmin):
+#     show_full_result_count = False
+#     change_form_template = 'src/templates/admin/change_form.html'
+#     search_fields = ('first_name', 'last_name', 'location__location')
+#     list_display = ('first_name', 'last_name', 'location', )
+#     actions = (ExportCsvMixin.export_as_csv, ExportAllCsvMixin.export_all_as_csv, )
+#     list_per_page = 50
+#     list_filter = (
+#         ('date', DateFieldListFilter),
+#     )
+#     list_max_show_all = False
+#
+#     def get_actions(self, request):
+#         actions = super().get_actions(request)
+#         if not request.user.is_superuser:
+#             if 'export_as_csv' in actions:
+#                 del actions['export_as_csv']
+#             if 'export_all_as_csv' in actions:
+#                 del actions['export_all_as_csv']
+#         return actions
+#
+#     def get_search_results(self, request, queryset, search_term):
+#         search_result = super().get_search_results(request, queryset, search_term)
+#         if not request.user.is_superuser:
+#             if len(search_term) != 7:
+#                 search_result = (ProcedureForm.objects.none(), False)
+#         return search_result
+#
+#     def get_readonly_fields(self, request, obj=None):
+#         read_only_fields = super().get_readonly_fields(request)
+#         if not obj:
+#             read_only_fields = ()
+#             if not request.user.is_superuser:
+#                 print(type(request.method))
+#         if request.user.is_superuser:
+#             read_only_fields = ()
+#         return read_only_fields
+#
+#     def get_actions(self, request):
+#         actions = super().get_actions(request)
+#         if not request.user.is_superuser:
+#             if 'export_as_csv' in actions:
+#                 del actions['export_as_csv']
+#             if 'export_all_as_csv' in actions:
+#                 del actions['export_all_as_csv']
+#         return actions
+#
+#     def get_form(self, request, obj=None, **kwargs):
+#         form = super().get_form(request, obj, **kwargs)
+#         return form
+#
+#     def get_list_display(self, request):
+#         listDisplay = super().get_list_display(request)
+#         if not request.user.is_superuser:
+#             self.list_per_page = 10
+#         else:
+#             self.list_per_page = 50
+#         return listDisplay
 
-    def get_actions(self, request):
-        actions = super().get_actions(request)
-        if not request.user.is_superuser:
-            if 'export_as_csv' in actions:
-                del actions['export_as_csv']
-            if 'export_all_as_csv' in actions:
-                del actions['export_all_as_csv']
-        return actions
 
-    def get_search_results(self, request, queryset, search_term):
-        search_result = super().get_search_results(request, queryset, search_term)
-        if not request.user.is_superuser:
-            if len(search_term) != 7:
-                search_result = (ProcedureForm.objects.none(), False)
-        return search_result
-
-    def get_readonly_fields(self, request, obj=None):
-        read_only_fields = super().get_readonly_fields(request)
-        if not obj:
-            read_only_fields = ()
-            if not request.user.is_superuser:
-                print(type(request.method))
-        if request.user.is_superuser:
-            read_only_fields = ()
-        return read_only_fields
-
-    def get_actions(self, request):
-        actions = super().get_actions(request)
-        if not request.user.is_superuser:
-            if 'export_as_csv' in actions:
-                del actions['export_as_csv']
-            if 'export_all_as_csv' in actions:
-                del actions['export_all_as_csv']
-        return actions
-
-    def get_form(self, request, obj=None, **kwargs):
-        form = super().get_form(request, obj, **kwargs)
-        return form
-
-    def get_list_display(self, request):
-        listDisplay = super().get_list_display(request)
-        if not request.user.is_superuser:
-            self.list_per_page = 10
-        else:
-            self.list_per_page = 50
-        return listDisplay
-
-
-class ProcedureFormAdmin(admin.ModelAdmin):
-    # list_filter = (
-    #     ('date', DateFieldListFilter),
-    # )
-    show_full_result_count = False
-    change_form_template = 'src/templates/admin/change_form.html'
-    search_fields = ('first_name', 'last_name')
-    list_display = ('first_name', 'last_name', 'age', 'mobile_number', 'procedure', 'area_of_treatment',
-                    'cost', 'result', 'remark', 'date'
-                    )
-    list_per_page = 50
-    readonly_fields = ('first_name', 'last_name', 'age', 'mobile_number', 'procedure', 'area_of_treatment',
-                    'cost', 'result', 'remark', 'date'
-                    )
-    list_max_show_all = False
-    actions = (ExportCsvMixin.export_as_csv, ExportAllCsvMixin.export_all_as_csv, )
-
-    def get_search_results(self, request, queryset, search_term):
-        search_result = super().get_search_results(request, queryset, search_term)
-        if not request.user.is_superuser:
-            if len(search_term) != 7:
-                search_result = (ProcedureForm.objects.none(), False)
-        return search_result
-
-    def get_readonly_fields(self, request, obj=None):
-        read_only_fields = super().get_readonly_fields(request)
-        if not obj:
-            read_only_fields = ()
-            if not request.user.is_superuser:
-                print(type(request.method))
-        if request.user.is_superuser:
-            read_only_fields = ()
-        return read_only_fields
-
-    def get_actions(self, request):
-        actions = super().get_actions(request)
-        if not request.user.is_superuser:
-            if 'export_as_csv' in actions:
-                del actions['export_as_csv']
-            if 'export_all_as_csv' in actions:
-                del actions['export_all_as_csv']
-        return actions
-
-    def get_form(self, request, obj=None, **kwargs):
-        form = super().get_form(request, obj, **kwargs)
-        if 'result' in form.base_fields:
-            form.base_fields['result'].widget.attrs['placeholder'] = "Did the patient get the results, etc.."
-        if 'remark' in form.base_fields:
-            form.base_fields['remark'].widget.attrs['placeholder'] = "e.g.: Burnt/Pain was not satisfied.."
-        return form
-
-    def get_list_display(self, request):
-        listDisplay = super().get_list_display(request)
-        if not request.user.is_superuser:
-            self.list_per_page = 10
-        else:
-            self.list_per_page = 50
-        return listDisplay
+# class ProcedureFormAdmin(admin.ModelAdmin):
+#     # list_filter = (
+#     #     ('date', DateFieldListFilter),
+#     # )
+#     show_full_result_count = False
+#     change_form_template = 'src/templates/admin/change_form.html'
+#     search_fields = ('first_name', 'last_name')
+#     list_display = ('first_name', 'last_name', 'age', 'mobile_number', 'procedure', 'area_of_treatment',
+#                     'cost', 'result', 'remark', 'date'
+#                     )
+#     list_per_page = 50
+#     readonly_fields = ('first_name', 'last_name', 'age', 'mobile_number', 'procedure', 'area_of_treatment',
+#                     'cost', 'result', 'remark', 'date'
+#                     )
+#     list_max_show_all = False
+#     actions = (ExportCsvMixin.export_as_csv, ExportAllCsvMixin.export_all_as_csv, )
+#
+#     def get_search_results(self, request, queryset, search_term):
+#         search_result = super().get_search_results(request, queryset, search_term)
+#         if not request.user.is_superuser:
+#             if len(search_term) != 7:
+#                 search_result = (ProcedureForm.objects.none(), False)
+#         return search_result
+#
+#     def get_readonly_fields(self, request, obj=None):
+#         read_only_fields = super().get_readonly_fields(request)
+#         if not obj:
+#             read_only_fields = ()
+#             if not request.user.is_superuser:
+#                 print(type(request.method))
+#         if request.user.is_superuser:
+#             read_only_fields = ()
+#         return read_only_fields
+#
+#     def get_actions(self, request):
+#         actions = super().get_actions(request)
+#         if not request.user.is_superuser:
+#             if 'export_as_csv' in actions:
+#                 del actions['export_as_csv']
+#             if 'export_all_as_csv' in actions:
+#                 del actions['export_all_as_csv']
+#         return actions
+#
+#     def get_form(self, request, obj=None, **kwargs):
+#         form = super().get_form(request, obj, **kwargs)
+#         if 'result' in form.base_fields:
+#             form.base_fields['result'].widget.attrs['placeholder'] = "Did the patient get the results, etc.."
+#         if 'remark' in form.base_fields:
+#             form.base_fields['remark'].widget.attrs['placeholder'] = "e.g.: Burnt/Pain was not satisfied.."
+#         return form
+#
+#     def get_list_display(self, request):
+#         listDisplay = super().get_list_display(request)
+#         if not request.user.is_superuser:
+#             self.list_per_page = 10
+#         else:
+#             self.list_per_page = 50
+#         return listDisplay
 
 
 admin.site.base_template = 'src/templates/admin/base.html'
@@ -338,7 +338,7 @@ admin.site.index_title = "Welcome to Radhe Krishna Repo Agency"
 # admin.site.register(Location)
 # admin.site.register(Procedure)
 # admin.site.register(GeneralEntry, GeneralEntryAdmin)
-admin.site.register(ProcedureForm, ProcedureFormAdmin)
+# admin.site.register(ProcedureForm, ProcedureFormAdmin)
 
 admin.site.register(AxisCompleteDetail, AxisCompleteDetailAdmin)
 admin.site.register(MahindraCompleteDetail, MahindraCompleteDetailAdmin)
--- src/models.py
+++ src/models.py
@@ -3,59 +3,59 @@ import datetime
 from phonenumber_field.modelfields import PhoneNumberField
 
 
-class Location(models.Model):
-    location = models.CharField(default="", max_length=30, unique=True)
+# class Location(models.Model):
+#     location = models.CharField(default="", max_length=30, unique=True)
+#
+#     def __str__(self):
+#         return self.location
+#
+#
+# class Procedure(models.Model):
+#     procedure = models.CharField(default="", max_length=30, unique=True)
+#
+#     def __str__(self):
+#         return self.procedure
+#
+#
+# class GeneralEntry(models.Model):
+#     first_name = models.CharField(default="", max_length=100)
+#     last_name = models.CharField(default="", max_length=100)
+#     mobile_number = models.CharField(default="", max_length=10)
+#     address = models.TextField()
+#     date = models.DateTimeField(default=datetime.datetime.now)
+#     location = models.ForeignKey(Location, on_delete=models.PROTECT)
+#
+#     class Meta:
+#         verbose_name = 'General Entry'
+#         verbose_name_plural = 'General Entry'
+#
+#     def __str__(self):
+#         return self.first_name + " " + self.last_name + " Location: " + self.location.location
+#
 
-    def __str__(self):
-        return self.location
-
-
-class Procedure(models.Model):
-    procedure = models.CharField(default="", max_length=30, unique=True)
-
-    def __str__(self):
-        return self.procedure
-
-
-class GeneralEntry(models.Model):
-    first_name = models.CharField(default="", max_length=100)
-    last_name = models.CharField(default="", max_length=100)
-    mobile_number = models.CharField(default="", max_length=10)
-    address = models.TextField()
-    date = models.DateTimeField(default=datetime.datetime.now)
-    location = models.ForeignKey(Location, on_delete=models.PROTECT)
-
-    class Meta:
-        verbose_name = 'General Entry'
-        verbose_name_plural = 'General Entry'
-
-    def __str__(self):
-        return self.first_name + " " + self.last_name + " Location: " + self.location.location
-
-
-class ProcedureForm(models.Model):
-    first_name = models.CharField(default="", max_length=100)
-    last_name = models.CharField(default="", max_length=100)
-    age = models.IntegerField(default="", max_length=2)
-    mobile_number = models.CharField(default="", max_length=10)
-    procedure = models.ForeignKey(Procedure, on_delete=models.PROTECT, blank=True)
-    area_of_treatment = models.TextField(default="", max_length=3000, blank=True)
-    cost = models.CharField(default="", max_length=2000, blank=True)
-    result = models.TextField(default="", max_length=4000, blank=True)
-    remark = models.TextField(default="", max_length=4000, blank=True)
-    no_of_session = models.IntegerField(default=0)
-    settings_dose = models.TextField(default="", max_length=20000)
-    date = models.DateTimeField(default=datetime.datetime.now)
-
-    class Meta:
-        verbose_name = 'Procedure Form'
-        verbose_name_plural = 'Procedure Forms'
-
-    def get_mobile_number(self):
-        return len(self.mobile_number) == 10
-
-    def __str__(self):
-        return self.first_name + " " + self.last_name + " age: " + str(self.age) + " procedure: " + self.procedure.procedure + " ,treatment: " + self.area_of_treatment
+# class ProcedureForm(models.Model):
+#     first_name = models.CharField(default="", max_length=100)
+#     last_name = models.CharField(default="", max_length=100)
+#     age = models.IntegerField(default="", max_length=2)
+#     mobile_number = models.CharField(default="", max_length=10)
+#     procedure = models.ForeignKey(Procedure, on_delete=models.PROTECT, blank=True)
+#     area_of_treatment = models.TextField(default="", max_length=3000, blank=True)
+#     cost = models.CharField(default="", max_length=2000, blank=True)
+#     result = models.TextField(default="", max_length=4000, blank=True)
+#     remark = models.TextField(default="", max_length=4000, blank=True)
+#     no_of_session = models.IntegerField(default=0)
+#     settings_dose = models.TextField(default="", max_length=20000)
+#     date = models.DateTimeField(default=datetime.datetime.now)
+#
+#     class Meta:
+#         verbose_name = 'Procedure Form'
+#         verbose_name_plural = 'Procedure Forms'
+#
+#     def get_mobile_number(self):
+#         return len(self.mobile_number) == 10
+#
+#     def __str__(self):
+#         return self.first_name + " " + self.last_name + " age: " + str(self.age) + " procedure: " + self.procedure.procedure + " ,treatment: " + self.area_of_treatment
 
 
 class AxisCompleteDetail(models.Model):
